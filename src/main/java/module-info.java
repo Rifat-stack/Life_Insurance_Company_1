9e4f291a937fbb8c9366ca499793dd903bd9f104
zarints05 <2311933@iub.edu.bd>
2025-08-01 17:48:33 +0600

    init and commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,5 +4,8 @@ module com.example.life_insurance_main {
 
 
     opens com.example.life_insurance_main to javafx.fxml;
+    opens com.example.life_insurance_main.ClaimOfficer to javafx.fxml, java.base;
+    opens com.example.life_insurance_main.Underwriter to javafx.fxml, java.base;
+
     exports com.example.life_insurance_main;
 }
