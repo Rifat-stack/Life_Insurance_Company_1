f773d9005462e5f1990e1376a21cfd9c55e87c17
Rifat Abdullah Khan <2311188@iub.edu.bd>
2025-08-02 00:05:54 +0600

    init commit

--- src/main/java/com/example/life_insurance_main/CustomerPayPremium.java
+++ src/main/java/com/example/life_insurance_main/CustomerPayPremium.java
@@ -3,5 +3,10 @@ package com.example.life_insurance_main;
 public class CustomerPayPremium
 {
     @javafx.fxml.FXML
+    private Label IDfiled;
+    @javafx.fxml.FXML
+    private Label AmountFiled;
+
+    @javafx.fxml.FXML
     public void initialize() {
     }}
