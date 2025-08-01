e95822859af549bca24f6ee1dcf76645f7d7040a
Atik-waheed <2231482@iub.edu.bd>
2025-08-01 15:34:55 +0600

    int

---
+++ src/main/java/com/example/life_insurance_main/BillingViewPaymentHistory.java
@@ -0,0 +1,7 @@
+package com.example.life_insurance_main;
+
+public class BillingViewPaymentHistory
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
---
+++ src/main/java/com/example/life_insurance_main/BillingViewTransactions.java
@@ -0,0 +1,7 @@
+package com.example.life_insurance_main;
+
+public class BillingViewTransactions
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
