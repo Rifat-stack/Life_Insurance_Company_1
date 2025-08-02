927b0e9c052cd000ee87eccf7365b14c109cb74a
Rifat Abdullah Khan <2311188@iub.edu.bd>
2025-08-02 13:53:41 +0600

    init commit

---
+++ src/main/java/com/example/life_insurance_main/InsuranceAgentPerformanceSummary.java
@@ -0,0 +1,22 @@
+package com.example.life_insurance_main;
+
+public class InsuranceAgentPerformanceSummary
+{
+    @javafx.fxml.FXML
+    private Label approvedApplicationsLabel;
+    @javafx.fxml.FXML
+    private Label commissionEarnedLabel;
+    @javafx.fxml.FXML
+    private Button GenarateReportButton;
+    @javafx.fxml.FXML
+    private BarChart performanceChart;
+    @javafx.fxml.FXML
+    private Label policiesSoldLabel;
+    @javafx.fxml.FXML
+    private ComboBox RangeSelectionCombo;
+    @javafx.fxml.FXML
+    private Label applicationsSubmittedLabel;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
