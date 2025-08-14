311cc4982b9f99160e133f1d0b676324bfc87a72
Rifat Abdullah Khan <2311188@iub.edu.bd>
2025-08-14 18:46:27 +0600

    init commit

---
+++ src/main/java/com/example/life_insurance_main/Customer.java
@@ -0,0 +1,18 @@
+package com.example.life_insurance_main;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Customer extends USER{
+
+    public Customer(String userId, String name, String email, String phone, String password) {
+        super(userId, name, email, phone, password);
+    }
+
+
+
+}
+
+
+
+
