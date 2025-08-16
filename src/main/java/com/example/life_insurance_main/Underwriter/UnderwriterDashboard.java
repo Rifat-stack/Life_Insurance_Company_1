5698a35ab003bd058ae5036f9063d502817d8a64
zarints05 <2311933@iub.edu.bd>
2025-08-16 14:48:20 +0600

    init and commit

--- src/main/java/com/example/life_insurance_main/Underwriter/UnderwriterDashboard.java
+++ src/main/java/com/example/life_insurance_main/Underwriter/UnderwriterDashboard.java
@@ -1,7 +1,74 @@
 package com.example.life_insurance_main.Underwriter;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class UnderwriterDashboard
 {
     @javafx.fxml.FXML
+    private TextField nameTF;
+    @javafx.fxml.FXML
+    private Button deferredApplicationsButton;
+    @javafx.fxml.FXML
+    private Button applicantSearchScreenButton;
+    @javafx.fxml.FXML
+    private Button evaluateNewApplicationButton;
+    @javafx.fxml.FXML
+    private Button autoEligibleButton;
+    @javafx.fxml.FXML
+    private Button insuranceUnderwritingPortalButton;
+    @javafx.fxml.FXML
+    private Button requestMedicalExamButton;
+    @javafx.fxml.FXML
+    private TextField idTF;
+    @javafx.fxml.FXML
+    private Label dBLabel;
+    @javafx.fxml.FXML
+    private Button pendingApplicationsButton;
+    @javafx.fxml.FXML
+    private Button signOutButton;
+    @javafx.fxml.FXML
+    private Button policyApplicationsButton;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void requestMedicalExamOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void applicantSearchScreenOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void deferredApplicationsOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void signOutOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void policyApplicationsOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void autoEligibleOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void pendingApplicationsOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void insuranceUnderwritingPortalOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void evaluateNewApplicationOA(ActionEvent actionEvent) {
+    }
+}
